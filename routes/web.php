c22793b1a88cc445de23a03ab557db630bac5ac3
Akram <akram.mustafaaa@gmail.com>
2026-01-04 12:37:25 +0200

    $jobs variable, but you are not passing view

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,12 @@ use Laravel\Fortify\Features;
 use Livewire\Volt\Volt;
 use Illuminate\Support\Arr;
 
+$jobs = [
+    ['id' => '1', 'title' => 'Frontend Developer',   'salary' => '$50.000',  'location' => 'New York, NY'],
+    ['id' => '2', 'title' => 'Backend Developer',    'salary' => '$78.000',  'location' => 'San Francisco, CA'],
+    ['id' => '3', 'title' => 'Full Stack Developer', 'salary' => '$952.300', 'location' => 'Remote']
+];
+
 Route::get('/welcome', function () {
     return view('welcome');
 })->name('home');
@@ -21,27 +27,22 @@ Route::get('/team', function () {
 Route::get('/', function () {
     return view('home', ['greeting' => 'Hello, welcome to our website!', 'name' => 'Larry Robaart']);
 });
-Route::get('/jobs', function () {
-    return view('jobs', [
-        'jobs' => [
-            ['id' => '1', 'title' => 'Frontend Developer',   'salary' => '$50.000',  'location' => 'New York, NY'],
-            ['id' => '2', 'title' => 'Backend Developer',    'salary' => '$78.000',  'location' => 'San Francisco, CA'],
-            ['id' => '3', 'title' => 'Full Stack Developer', 'salary' => '$952.300', 'location' => 'Remote']
-        ]
-    ]);
+
+
+//create jobs route and use jobs array to show jobs
+Route::get('/jobs', function () use ($jobs) {
+
+    return view('jobs', ['jobs' => $jobs]);
 });
-Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-        ['id' => '1', 'title' => 'Frontend Developer',   'salary' => '$50.000',  'location' => 'New York, NY'],
-        ['id' => '2', 'title' => 'Backend Developer',    'salary' => '$78.000',  'location' => 'San Francisco, CA'],
-        ['id' => '3', 'title' => 'Full Stack Developer', 'salary' => '$952.300', 'location' => 'Remote']
-    ];
+
+//clear codely show job by id
+Route::get('/jobs/{id}', function ($id) use ($jobs) {
     $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
 
-    abort_if(!$job, 404);
+    abort_if(!$job, 'about');
 
     return view('job', ['job' => $job]);
-})->where('id', '[0-9]+');
+});
 
 Route::get('/akram', function () {
     return ['feoo' => 'bar'];
